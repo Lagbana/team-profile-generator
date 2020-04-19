d36fa722d861dc9fea57a745583c742a07949be2
Larry_A <larryagbana@gmail.com>
2020-04-19 18:26:13 -0400

    create team manager func

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -12,37 +14,38 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
 // Inquirer questions
 const newEmployeeQuestions = [
     {
         type: 'input',
         name: 'employeeName',
-        message: 'What is your full name?',
-        validate: function(text) {
+        message: "What is the employee's full name?",
+        validate: function (text) {
             if (text.length < 1) {
-              return 'Please enter a your full name';
+                return "Please enter the employee's full name";
             }
             return true;
-          },
-          filter: String
+        },
+        filter: String
     },
     {
         type: 'input',
         name: 'id',
-        message: 'What is your employee id?',
-        validate: function(value) {
+        message: "What is your employee's id?",
+        validate: function (value) {
             const valid = (Number.isInteger(value) && (value !== 0))
             return valid || 'Please press the up arrow button and enter a valid number'
-          },
+        },
         filter: Number
     },
     {
         type: 'input',
         name: 'email',
-        message: "What's your work email?",
-        validate: function(value) {
-        const regex = /^[a-zA-Z0-9_\.-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9\.]{2,5}$/i
-        return regex.test(value) || 'Please enter a valid email address'
+        message: "What is your employee's work email?",
+        validate: function (value) {
+            const regex = /^[a-zA-Z0-9_\.-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9\.]{2,5}$/i
+            return regex.test(value) || 'Please enter a valid email address'
         },
         filter: String
     },
@@ -55,19 +58,14 @@ const newEmployeeQuestions = [
 ]
 
 // Questionaire function using inquirer
-const roleQuestionaire = async () => {
+const employeeResponses = async () => {
     try {
-        return response = await inquirer.prompt(newEmployeeQuestions)
+        const response = await inquirer.prompt(newEmployeeQuestions)
+        return response
     } catch (err) {
         console.log(err)
     }
 }
-const getResult = async () => {
-    const response = await roleQuestionaire()
-    return response
-}
-
-
 
 // Extra Information
 let engineerDetail = [
@@ -75,13 +73,13 @@ let engineerDetail = [
         type: 'input',
         name: 'github',
         message: 'What is your GitHub username?',
-        validate: function(text) {
+        validate: function (text) {
             if (text.length < 1) {
-              return 'Please enter a valid GitHub username';
+                return 'Please enter a valid GitHub username';
             }
             return true;
-          },
-          filter: String
+        },
+        filter: String
     }
 ]
 let internDetail = [
@@ -89,13 +87,13 @@ let internDetail = [
         type: 'input',
         name: 'school',
         message: 'What is the name of your current school?',
-        validate: function(text) {
+        validate: function (text) {
             if (text.length < 1) {
-              return 'Please enter your current school name';
+                return 'Please enter your current school name';
             }
             return true;
-          },
-          filter: String
+        },
+        filter: String
     }
 ]
 let managerDetail = [
@@ -111,32 +109,113 @@ let managerDetail = [
     }
 ]
 
-
 // Role Detail Questionaire function
 const roleDetail = async () => {
-    const {employeeName, id, email, role} = await getResult()
-
+    const { employeeName, id, email, role } = await employeeResponses()
     try {
         if (role === 'engineer') {
             const response = await inquirer.prompt(engineerDetail)
             const detail = response['github']
-            console.log({employeeName, id, email, role, detail})
-            // return
-        } else if (role === 'intern'){
+            return ({ employeeName, id, email, role, detail })
+        } else if (role === 'intern') {
             const response = await inquirer.prompt(internDetail)
             const detail = response['school']
-            console.log({employeeName, id, email, role, detail})
-        } else if (role === 'manager'){
+            return ({ employeeName, id, email, role, detail })
+        } else if (role === 'manager') {
             const response = await inquirer.prompt(managerDetail)
             const detail = response['officeNumber']
-            console.log({employeeName, id, email, role, detail})
+            return ({ employeeName, id, email, role, detail })
         }
     } catch (err) {
         console.log(err)
     }
 }
 
-roleDetail()
+const numberOfNewEmployees = async () => {
+    let numberOfEmployees = [
+        {
+            type: 'input',
+            name: 'number',
+            message: `How many 'Non-Managerial' employees do you need to create (has to be at least 2)?`,
+            validate: function (value) {
+                const valid = (Number.isInteger(value) && (value !== 0) && (value !== 1))
+                return valid || 'Please press the up arrow button and enter a valid number'
+            },
+            filter: Number
+        }
+    ]
+    const number = await inquirer.prompt(numberOfEmployees)
+    return number
+}
+
+const createTeamManager = async () => {
+    let isManager, managerName, managerID, managerEmail, managerDetail
+
+    while (isManager !== 'manager') {
+        console.log(`
+        !!! Warning: You need to create a manager first.
+        `)
+        const response = await roleDetail()
+        const {employeeName, id, email, role, detail} = response
+        managerName = employeeName
+        managerID = id
+        managerEmail = email
+        managerDetail = detail
+        isManager = role
+    }
+    const manager = new Manager(managerName, managerID, managerEmail, managerDetail)
+    return manager
+}
+
+// Create function that prompts user for names and role, 
+const teamBuilder = async () => {
+    let result = []
+    console.log(`
+            *** Employee Profile Generator ***
+
+    Please follow the prompts below to build your team profile
+    `)
+
+    console.log(`
+                    *********************
+                    New Manager Creation
+                    *********************
+        `)
+    const manager = await createTeamManager() 
+    result.push(manager)
+
+    const response = await numberOfNewEmployees()
+    const { number } = response
+
+    console.log(`
+    !!! Warning: To prevent restart, only create non-managers in the next section.
+    `)
+
+    for (let i = 0; i < number; i++) {
+        console.log(`
+                    *********************
+                    New Employee Creation
+                    *********************
+        `)
+
+        const response = await roleDetail()
+        const { employeeName, id, email, role, detail } = response
+
+        if ((role === 'engineer') && (role !== 'manager')) {
+            const engineer = new Engineer(employeeName, id, email, detail)
+            result.push(engineer)
+        } else if ((role === 'intern') && (role !== 'manager')) {
+            const intern = new Intern(employeeName, id, email, detail)
+            result.push(intern)
+        } else {
+            console.log("You can not have '2' managers in one team, please start again.")
+            return
+        }
+    }
+    console.log(result)
+    // console.log(result[0].name)
+}
+teamBuilder()
 
 
 // After the user has input all employees desired, call the `render` function (required
